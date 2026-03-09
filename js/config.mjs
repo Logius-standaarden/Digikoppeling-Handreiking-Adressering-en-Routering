ef6d33d0b847964987b79b97453f5ed8df08d3eb
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2026-03-09 10:19:37 +0100

    Beheer: verwijder PDF formaat

--- js/config.mjs
+++ js/config.mjs
@@ -1,10 +1,6 @@
 import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
 
 loadRespecWithConfiguration({
-  alternateFormats: [ { 
-        "label" : "pdf",
-        "uri" : "Digikoppeling-Handreiking-Adressering-en-Routering.pdf"
-      } ],
   authors: [ { 
         "company" : "Logius",
         "companyURL" : "http://www.logius.nl/",
